1d7534e3c381ef95e77987bffd68b85cdffd77d8
chai2010 <chaishushan@gmail.com>
2015-12-16 10:56:29 +0800

    rebuild

---
+++ make.bat
@@ -0,0 +1,6 @@
+:: Copyright 2015 <chaishushan{AT}gmail.com>. All rights reserved.
+:: Use of this source code is governed by a BSD-style
+:: license that can be found in the LICENSE file.
+
+mingw32-make
+
